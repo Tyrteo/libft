d1dc3b47f50ee4a09e795a9e8e53658101018325
Martin Rodriguez Gomez <martrod2@c3r2s5.42urduliz.com>
2026-01-26 18:59:07 +0100

    finished ft_strlen

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,8 +6,31 @@
 /*   By: martrod2 <martrod2@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/25 19:01:53 by martrod2          #+#    #+#             */
-/*   Updated: 2026/01/25 19:02:01 by martrod2         ###   ########.fr       */
+/*   Updated: 2026/01/26 18:54:29 by martrod2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft";
 
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (*s)
+	{
+		i++;
+		s++;
+	}
+	return (i);
+}
+/*
+#include <stdio.h>
+
+int	main(void)
+{
+	char *testString = "string de prueba";
+
+	printf("%ld", ft_strlen(testString));
+}
+*/
--- libft.h
+++ libft.h
@@ -6,18 +6,21 @@
 /*   By: martrod2 <martrod2@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/23 17:49:10 by martrod2          #+#    #+#             */
-/*   Updated: 2026/01/25 19:05:54 by martrod2         ###   ########.fr       */
+/*   Updated: 2026/01/26 18:47:09 by martrod2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
+#include <stddef.h>
+
 int	ft_isalpha(int c);
 int	ft_isdigit(int c);
 int	ft_isalnum(int c);
-int ft_isascii(int c);
-int ft_isprint(int c);
+int	ft_isascii(int c);
+int	ft_isprint(int c);
 size_t	ft_strlen(const char *s);
+void	*memset(void *s, int c, size_t n)
 
 #endif
